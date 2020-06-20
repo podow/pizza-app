2a935da2776fe3400832f58848dc1095e075a5ae
bogdanberdnikov <bogdan.b@legalbet.com>
2020-06-21 00:08:40 +0600

    refactor: remove unused code

--- server/server.js
+++ server/server.js
@@ -31,69 +31,6 @@ app.prepare().then(() => {
     return !dev ? ssrCache({ req, res, pagePath }) : app.render(req, res, '/');
   });
 
-  server.get('/project/:slug', (req, res) => {
-    return app.render(req, res, '/project', { slug: req.params.slug });
-  });
-
-  server.get('/news/:slug', (req, res) => {
-    const queryParams = {
-      slug: req.params.slug,
-      category_id: req.params.category_id
-    };
-    const pagePath = '/news';
-
-    return !dev
-      ? ssrCache({ req, res, pagePath, queryParams })
-      : app.render(req, res, '/news', {
-          slug: req.params.slug,
-          category_id: req.params.category_id
-        });
-  });
-
-  server.get('/flat/:id', (req, res) => {
-    const queryParams = { id: req.params.id };
-    const pagePath = '/flat';
-
-    return !dev
-      ? ssrCache({ req, res, pagePath, queryParams })
-      : app.render(req, res, '/flat', { id: req.params.id });
-  });
-
-  server.get('/flatslist/:slug', (req, res) => {
-    const queryParams = { slug: req.params.slug };
-    const pagePath = '/flatslist';
-
-    return !dev
-      ? ssrCache({ req, res, pagePath, queryParams })
-      : app.render(req, res, '/flatslist', {
-          slug: req.params.slug
-        });
-  });
-
-  server.get('/perspective/:slug', (req, res) => {
-    const queryParams = { slug: req.params.slug };
-    const pagePath = '/perspective';
-
-    return !dev
-      ? ssrCache({ req, res, pagePath, queryParams })
-      : app.render(req, res, '/perspective', { slug: req.params.slug });
-  });
-
-  server.get('/about', (req, res) => {
-    const pagePath = '/about';
-
-    return !dev
-      ? ssrCache({ req, res, pagePath })
-      : app.render(req, res, '/about');
-  });
-
-  server.get('/contacts', (req, res) => {
-    const pagePath = '/contacts';
-    return !dev
-      ? ssrCache({ req, res, pagePath })
-      : app.render(req, res, '/contacts');
-  });
-
   server.get('*', (req, res) => handle(req, res));
 
   server.listen(port, error => {
