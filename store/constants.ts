e5dbc055aacf3693a5043c46ba68169ff928ed07
bogdanberdnikov <bogdan.b@legalbet.com>
2020-06-22 16:43:40 +0600

    feat: add common store

--- store/constants.ts
+++ store/constants.ts
@@ -8,4 +8,8 @@ export const DELETE = '_DELETE';
 export const EDIT = '_EDIT';
 export const SET = '_SET';
 
+export const TOGGLE_MODAL = 'common/TOGGLE_MODAL';
+export const INCREMENT_API = 'common/INCREMENT_API';
+export const DECREMENT_API = 'common/DECREMENT_API';
+
 export const PRODUCTS = 'products/PRODUCTS';
