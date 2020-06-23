8226798ed9ebc82676dc084e8313a23e077ce3fe
bogdanberdnikov <bogdan.b@legalbet.com>
2020-06-24 02:34:07 +0600

    test

--- app/pages/Main/ProductsBlock/index.tsx
+++ app/pages/Main/ProductsBlock/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { ProductsSection, ProductsSectionItemsWrapper } from './styles';
@@ -10,17 +10,24 @@ import { IProduct } from 'interfaces/components/products';
 import { Container } from 'app/components/container';
 import ProductModal from './ProductModal';
 import ProductCard from './ProductCard';
+import { fetchProducts } from '../../../../store/products/actions';
 
 const ProductsBlock = () => {
   const products = useSelector(state => state.products.data);
   const [activeProduct, setActiveProduct] = useState({} as IProduct);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
   const clickHandler = id => {
     const product: any = products.find(product => product.id === id);
     setActiveProduct(product);
     dispatch(toggleModal({ name: 'productModal', open: true }));
   };
+
+  console.log(products);
 
   return (
     <ProductsSection>
