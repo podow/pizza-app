3abcf28b4fabf2ee8bcaf61d0aa827bd86c8de2a
bogdanberdnikov <bogdan.b@legalbet.com>
2020-06-23 23:52:36 +0600

    feat: add style variables

---
+++ app/variables.ts
@@ -0,0 +1,6 @@
+export const mainColor = '#be6c2f';
+export const darkHover = '#ad632d';
+export const lightHover = '#ce7533';
+
+export const mainTransitionTime = '0.3s';
+export const longTransitionTime = '0.5s';
