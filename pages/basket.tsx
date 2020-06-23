3a65f2aa432f60455e5bbcebd929521af85c3d97
bogdanberdnikov <bogdan.b@legalbet.com>
2020-06-23 23:53:16 +0600

    feat: start Basket component & page

---
+++ pages/basket.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+import Wrapper from '../layout';
+
+const BasketPage: NextPage = () => {
+  return <Wrapper>Basket</Wrapper>;
+};
+
+export default BasketPage;
