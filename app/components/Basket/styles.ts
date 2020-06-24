9e5342f8704375626db8195362b799d00055c0d7
bogdanberdnikov <bogdan.b@legalbet.com>
2020-06-25 00:55:59 +0600

    feat(Basket): add dropdown

--- app/components/Basket/styles.ts
+++ app/components/Basket/styles.ts
@@ -1,5 +1,45 @@
 import styled from 'styled-components';
 import { darkHover, mainColor, mainTransitionTime } from 'app/variables';
+import { triangle } from '../../mixins';
+
+export const BasketWrapper = styled.div`
+  position: relative;
+
+  &:hover {
+    .dropdown {
+      display: block;
+    }
+  }
+
+  .dropdown {
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 0 5px #dedede;
+
+    // display: none;
+    padding: 0 16px;
+    width: 250px;
+
+    position: absolute;
+    top: 50px;
+    right: 0;
+    z-index: 999;
+
+    &:after {
+      content: '';
+      ${triangle('20px', { top: '-10px', right: '0' })}
+    }
+
+    ul {
+      padding: 24px 0 16px;
+    }
+
+    .total {
+      padding: 16px 0 24px;
+      border-top: 1px solid #dedede;
+    }
+  }
+`;
 
 export const BasketLink = styled.a`
   display: flex;
