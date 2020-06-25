0f22eb05a6a4176e6f2a83c54ab08e7d25190198
bogdanberdnikov <bogdan.b@legalbet.com>
2020-06-26 00:00:42 +0600

    fix(Product): add ingredients from api

--- store/products/adapter.ts
+++ store/products/adapter.ts
@@ -6,7 +6,13 @@ export const productAdapter = (product: IProduct) => ({
   discountPrice: product.discountPrice
     ? product.price - product.discountPrice
     : null,
-  image: process.env.API_HOST + product.image,
+  image: product.image
+    ? process.env.API_HOST + product.image
+    : '/static/images/product-placeholder.svg',
+  ingredients: product.ingredients.map(ingredient => ({
+    ...ingredient,
+    image: process.env.API_HOST + ingredient.image
+  })),
   count: 1
 });
 
