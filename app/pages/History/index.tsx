a3cd3fdab133dd151e7a609f9e106b6f8aeb2736
bogdanberdnikov <bogdan.b@legalbet.com>
2020-06-28 22:03:33 +0600

    feat(HistoryPage): start order history table

--- app/pages/History/index.tsx
+++ app/pages/History/index.tsx
@@ -1,14 +1,60 @@
 import React from 'react';
 
-import { HistoryPageWrapper } from './styles';
+import {
+  HistoryPageWrapper,
+  HistoryTableWrapper,
+  HistoryTable
+} from './styles';
 
 import { Container } from 'app/components/container';
 
 const HistoryContainer = () => {
+  const history = [
+    {
+      id: 38,
+      order_dt: '2020-06-28 11:22:11',
+      order_data:
+        '{"products":[{"id":"2","name":"Сырная","description":"Томатный соус, моцарелла","price":"10.00","discountPrice":7.5,"image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/cd3416f2eb57a1b53f03493938377c08.jpg","ingredients":[{"id":1,"name":"Mushrooms","image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/75818ed6cd5e47f4057de2031a1a5ca2.png","price":"1.5"},{"id":3,"name":"Jalapenos","image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/bb1ef8b587ad83e715d7ce269066df87.png","price":"1.2"}],"count":2},{"id":"1","name":"Аррива!","description":"Томаты, чоризо халяль, сладкий перец, красный лук, чеснок сухой, моцарелла, соус ранч, соус бургер, цыпленок","price":"10.00","discountPrice":null,"image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/f7a2572cf49aa5e56143c3fd5d194d2f.jpeg","ingredients":[{"id":1,"name":"Mushrooms","image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/75818ed6cd5e47f4057de2031a1a5ca2.png","price":"1.5"},{"id":2,"name":"Cheese","image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/c0fca7ac17029b0fdbd9b315a1e6d531.png","price":"2"},{"id":3,"name":"Jalapenos","image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/bb1ef8b587ad83e715d7ce269066df87.png","price":"1.2"}],"count":1},{"id":"3","name":"Кисло-сладкий цыпленок","description":"Кисло-сладкий соус, моцарелла, цыпленок, томатный соус","price":"15.00","discountPrice":10,"image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/632f86c842776bfcf5c3ec35ac0e5b7d.jpg","ingredients":[{"id":3,"name":"Jalapenos","image":"https://innoscripta-pizza-back-podow.herokuapp.com/backend/uploads/bb1ef8b587ad83e715d7ce269066df87.png","price":"1.2"}],"count":1}],"totalCost":"35"}'
+    }
+  ];
+
   return (
     <HistoryPageWrapper>
       <Container>
         <h2>History</h2>
+        <HistoryTableWrapper>
+          <HistoryTable>
+            <thead>
+              <tr>
+                <th>Order number</th>
+                <th>Date</th>
+                <th>Total</th>
+                <th>Status</th>
+                <th>Controls</th>
+              </tr>
+            </thead>
+            <tbody>
+              {history.map(({ id, order_dt, order_data }) => {
+                const { totalCost } = JSON.parse(order_data);
+
+                return (
+                  <tr key={id}>
+                    <td>{id}</td>
+                    <td>{order_dt}</td>
+                    <td>$ {totalCost}</td>
+                    <td>
+                      <span>Delivered</span>
+                    </td>
+                    <td>
+                      <button>Details</button>
+                      <button>Repeat order</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </HistoryTable>
+        </HistoryTableWrapper>
       </Container>
     </HistoryPageWrapper>
   );
--- app/pages/History/styles.ts
+++ app/pages/History/styles.ts
@@ -1,3 +1,65 @@
 import styled from 'styled-components';
+import { device } from 'app/breakpoints';
+import { lightHover, mainColor, mainTransitionTime } from 'app/variables';
 
-export const HistoryPageWrapper = styled.div``;
+export const HistoryPageWrapper = styled.div`
+  padding: 32px 0;
+
+  h2 {
+    margin-bottom: 20px;
+  }
+`;
+
+export const HistoryTableWrapper = styled.div`
+  overflow-y: auto;
+`;
+
+export const HistoryTable = styled.table`
+  min-width: 600px;
+  border-collapse: collapse;
+
+  @media ${device.tablet} {
+    width: 100%;
+  }
+
+  th {
+    text-align: left;
+
+    &:last-child {
+      text-align: right;
+    }
+  }
+
+  td {
+    border-bottom: 1px solid #dedede;
+    padding: 12px 0;
+    
+    span {
+      color: #fff;
+      background-color: #64be2f;
+      border-radius: 4px;
+      padding: 4px;
+    }
+
+    &:last-child {
+      text-align: right;
+    }
+    
+    button {
+      background-color: ${mainColor};
+      border-radius: 8px;
+      border: none;
+      color: #fff;
+      cursor: pointer;
+      padding: 6px 12px;
+      transition: background-color ${mainTransitionTime};
+      
+      &:hover {
+        background-color: ${lightHover};
+      }
+    
+      & ~ button {
+        margin-left: 8px;
+      }
+    }
+`;
